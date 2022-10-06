3e2e87bc82e77887c84869686177eec6f32885a0
Davi-web <jeseung.ha@vanderbilt.edu>
2022-10-06 01:06:00 -0500

    Fixed navbar

--- src/pages/components/header.tsx
+++ src/pages/components/header.tsx
@@ -4,18 +4,18 @@ import Link from "next/link";
 
 const Header: NextComponentType = () => {
 return(
-<div className="grid  text-center grid-cols-6 gap-6 rounded px-2 border border-black">
+<div className="grid text-center grid-cols-6 gap-6 rounded px-2 border border-black">
     <div className="flex justify-start align-middle">
-    <Image src="/../public/favicon.ico" alt="" className="w-8 rounded flex-shrink-0" height={50} width={50}/>
+    <Image src="/../public/favicon.ico" alt="" className="w-8 rounded flex-shrink-1" height={50} width={50}/>
     </div>
     <div className="pt-3">
-        <Link href={"/event/schedule"}>Event Schedule</Link>
+        <Link href={"/event/schedule"}>Schedules</Link>
     </div>
     <div className="pt-3">
-        <Link href={"/about"}>How it works</Link>   
+        <Link href={"/about"}>About</Link>   
     </div>
         <div className="pt-3">
-    <Link href={"/joinGroupChat"}>Join a Sports Community</Link>
+    <Link href={"/joinGroupChat"}>Community</Link>
         </div>
     <div className="pt-3">
         <Link href={"/login"}>Log In</Link>
