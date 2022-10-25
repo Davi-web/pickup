dc19d045a8ba481c5773035f1e3991ab38e88ec6
Anthony Lee <anthony.j.lee@vanderbilt.edu>
2022-10-25 18:57:46 -0500

    event stash

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,31 +1,43 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import TimePicker from '@mui/lab/TimePicker';
 import DateTimePicker from '@mui/lab/DateTimePicker';
 import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+import MobileDatePicker from '@mui/lab/MobileDatePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
  
-const Events = () => {
-  const [value, setValue] = React.useState(new Date('2014-08-18T21:11:54'));
- 
-  const handleChange = (newValue:any) => {
+export default function Events() {
+  const [value, setValue] = React.useState<AdapterDayjs | null>(new AdapterDayjs);
+
+  const handleChange = (newValue: AdapterDayjs | null) => {
     setValue(newValue);
   };
- 
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-    <DateTimePicker
-        renderInput={(props:any) => <TextField {...props} />}
-        label="DateTimePicker"
-        value={value}
-        onChange={(newValue:any) => {
-        setValue(newValue);
-        }}/>
+      <Stack spacing={3}>
+        <DateTimePicker
+          label="Date&Time picker"
+          value={value}
+          onChange={handleChange}
+          renderInput={(params:any) => <TextField {...params} />}
+        />
+      </Stack>
     </LocalizationProvider>
   );
+ 
+//   return (
+//     <div>Hello
+//     <LocalizationProvider dateAdapter={AdapterDayjs}>
+//     <DateTimePicker
+//         renderInput={(props:any) => <TextField {...props} />}
+//         label="DateTimePicker"
+//         value={value}
+//         onChange={(newValue:any) => {
+//         setValue(newValue);
+//         }}/>
+//     </LocalizationProvider>
+//     </div>
+//   );
 }
-
-export default Events;
