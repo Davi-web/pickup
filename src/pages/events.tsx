07879a5bb672ec2fb2f05682561740b631828f37
Davi-web <jeseung.ha@vanderbilt.edu>
2022-10-26 18:17:56 -0500

    Posted events for data with dummy data

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,52 +1,73 @@
-import React, {useState} from 'react';
-import dayjs, { Dayjs } from 'dayjs';
+import {useState} from 'react';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import { trpc } from '../utils/trpc';
  
 const Events = () => {
-  const [value, setValue] = useState(new Date('2014-08-18T21:11:54'));
+  const [value, setValue] = useState(Date.now());
+  const events = trpc.useMutation(['events.post-events'], {
+    onSuccess: () => {
+      console.log('success');
+    }
+  })
+
+
  
   const handleChange = (newValue:any) => {
-    setValue(newValue);
+    console.log(Date.parse(newValue));
+    setValue(Date.parse(newValue));
   };
- 
+  const onSubmit = () => {
+    events.mutate({
+      
+        name: 'test',
+        description: 'tst',
+        postedDate: new Date(Date.now()),
+        eventDate: new Date(value),
+        postedBy: "1",
+        eventLocation:"Rand",
+
+    })
+  }
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <Stack spacing={3}>
-                    <DesktopDatePicker
-                    label="Date desktop"
-                    inputFormat="MM/DD/YYYY"
-                    value={value}
-                    onChange={handleChange}
-                    renderInput={(params) => <TextField {...params} />}
-                    />
-                    <MobileDatePicker
-                    label="Date mobile"
-                    inputFormat="MM/DD/YYYY"
-                    value={value}
-                    onChange={handleChange}
-                    renderInput={(params) => <TextField {...params} />}
-                    />
-                    <TimePicker
-                    label="Time"
-                    value={value}
-                    onChange={handleChange}
-                    renderInput={(params) => <TextField {...params} />}
-                    />
-                    <DateTimePicker
-                    label="Date&Time picker"
-                    value={value}
-                    onChange={handleChange}
-                    renderInput={(params) => <TextField {...params} />}
-                    />
-                </Stack>
-            </LocalizationProvider>
+    <div className=' flex flex-col'>
+      <div className='flex justify-center pb-3'>
+      <h2>Enter Events Description</h2>
+
+      </div>
+      <div className='flex justify-center'>
+        <LocalizationProvider dateAdapter={AdapterDayjs} className=" flex justify-center">
+          <Stack spacing={3} >
+            
+              <MobileDatePicker
+              label="Date mobile"
+              inputFormat="MM/DD/YYYY"
+              value={value}
+              onChange={handleChange}
+              renderInput={(params) => <TextField {...params} />}
+              
+              />
+              <TimePicker
+              label="Time"
+              value={value}
+              onChange={handleChange}
+              renderInput={(params) => <TextField {...params} />}
+              />
+            
+          </Stack>
+        </LocalizationProvider>
+      </div>
+      <div className="flex justify-center pt-2">
+     <button onClick={onSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+        
+      </div>
+      
+    </div>
+    
   );
 }
 
