bb413f34d3c6a8eae6a17c91c6abe81198867d53
Anthony Lee <anthony.j.lee@vanderbilt.edu>
2022-10-26 18:21:47 -0500

    events creation page

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,13 +1,8 @@
 import React, {useState} from 'react';
-import dayjs, { Dayjs } from 'dayjs';
-import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
  
 export default function Events() {
   const [value, setValue] = useState<AdapterDayjs | null>(new AdapterDayjs);
@@ -16,36 +11,35 @@ export default function Events() {
     setValue(newValue);
   };
 
+  const [description, setDescription] = useState("");
+  const [eventName, setEventName] = useState("");
+  const [location, setLocation] = useState("");
+
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Stack spacing={3}>
-          <DesktopDatePicker
-          label="Date desktop"
-          inputFormat="MM/DD/YYYY"
-          value={value}
-          onChange={handleChange}
-          renderInput={(params) => <TextField {...params} />}
-          />
-          <MobileDatePicker
-          label="Date mobile"
-          inputFormat="MM/DD/YYYY"
-          value={value}
-          onChange={handleChange}
-          renderInput={(params) => <TextField {...params} />}
-          />
-          <TimePicker
-          label="Time"
-          value={value}
-          onChange={handleChange}
-          renderInput={(params) => <TextField {...params} />}
-          />
+    <div className="register">
+    <h1>Create an event to play with your friends!</h1>
+    <form action="/api/events" method="post" onSubmit={e => e.preventDefault()}>
+        <label htmlFor="Event Name"></label>
+        <input type="text" name="Event Name" placeholder="Enter event name" id="event-name" value={eventName} onChange={e=>setEventName(e.target.value)} required/>
+
+        <label htmlFor="Event Description"></label>
+        <input type="text" name="Event Description" placeholder="Enter description" id="event-description" value={description} onChange={e=>setDescription(e.target.value)}/>
+
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DateTimePicker
-          label="Date&Time picker"
-          value={value}
-          onChange={handleChange}
-          renderInput={(params) => <TextField {...params} />}
-          />
-      </Stack>
-    </LocalizationProvider>
-  );
+            label="Select a date and time..."
+            value={value}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} required/>}
+            />
+        </LocalizationProvider>
+
+        <label htmlFor="Location"></label>
+        <input type="text" name="Event Location" placeholder="Enter location" id="location" value={location} onChange={e=>setLocation(e.target.value)}/>
+
+        <input type="submit" value="Submit"/>
+    </form>
+    <div id="errorMsg" className=" text-red-600 text-lg"/>
+    </div>
+    );
 }
