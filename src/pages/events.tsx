b31f27e8276d208b803f7d9de02aaed05fb1b8dd
Anthony Lee <anthony.j.lee@vanderbilt.edu>
2022-10-27 12:12:07 -0500

    fixing events

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -6,9 +6,15 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
 import { trpc } from '../utils/trpc';
+import moment from 'moment-timezone'
  
 export default function Events() {
+
   const [value, setValue] = useState(Date.now());
+  const [eventName, setEventName] = useState("");
+  const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
+
   const events = trpc.useMutation(["events.post-events"], {
     onSuccess: () => {
       console.log('success');
@@ -22,25 +28,34 @@ export default function Events() {
 
   const onSubmit = () => {
     events.mutate({
-        name: 'test',
-        description: 'tst2',
+        name: eventName,
+        description: description,
         postedDate: new Date(Date.now()),
         eventDate: new Date(value),
         postedBy: "5",
-        eventLocation:"Rand",
+        eventLocation: location,
     })
   }
 
   return (
     <div className='flex flex-col'>
-      <div className='flex justify-center pb-3'>
-      <h2>Enter Events Description</h2>
+      <div className='flex justify-center pb-3'></div>
 
-      </div>
+      <form className='register'>
+        <label htmlFor="Event Name"></label>
+        <input type="text" name="eventName" placeholder="Enter event name" id="eventName" value={eventName} onChange={e=>setEventName(e.target.value)} required/>
+
+        <label htmlFor="Event Description"></label>
+        <input type="text" name="eventDescription" placeholder="Enter event description" id="description" value={description} onChange={e=>setDescription(e.target.value)}/>
+
+        <label htmlFor="Event Location"></label>
+        <input type="text" name="eventLocation" placeholder="Enter event location" id="location" value={location} onChange={e=>setLocation(e.target.value)}/>
+
+      </form>
+
       <div className='flex justify-center'>
         <LocalizationProvider dateAdapter={AdapterDayjs} className=" flex justify-center">
           <Stack spacing={3} >
-            
               <MobileDatePicker
               label="Date mobile"
               inputFormat="MM/DD/YYYY"
@@ -55,7 +70,6 @@ export default function Events() {
               onChange={handleChange}
               renderInput={(params) => <TextField {...params} />}
               />
-            
           </Stack>
         </LocalizationProvider>
       </div>
