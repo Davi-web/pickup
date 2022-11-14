e0c38c28cf541aedcfcd399d73efc14b1f5cba69
Davi-web <jeseung.ha@vanderbilt.edu>
2022-11-14 02:58:26 -0600

    changed a tag

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 type TechnologyCardProps = {
     name: string;
     description: string;
@@ -13,13 +15,13 @@ type TechnologyCardProps = {
       <section className="flex flex-col justify-center rounded border-2 border-gray-500 p-6 shadow-xl duration-500 motion-safe:hover:scale-105">
         <h2 className="text-lg text-gray-700">{name}</h2>
         <p className="text-sm text-gray-600">{description}</p>
-        <a
+        <Link
           className="m-auto mt-3 w-fit text-sm text-violet-500 underline decoration-dotted underline-offset-2"
           href={documentation}
           rel="noreferrer"
         >
           Click Here
-        </a>
+        </Link>
       </section>
     );
   };
