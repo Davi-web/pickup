fcc175448c621bd3eae3afce65487714f1f2f66b
Davi-web <jeseung.ha@vanderbilt.edu>
2022-11-15 02:28:24 -0600

    Deploy

--- src/utils/messageHandler.tsx
+++ src/utils/messageHandler.tsx
@@ -1,8 +1,13 @@
-export default (io, socket) => {
-    const createdMessage = (msg) => {
+const messageHandler = (io:any, socket:any ) => {
+    const createdMessage = (msg:any) => {
       socket.broadcast.emit("newIncomingMessage", msg);
     };
   
     socket.on("createdMessage", createdMessage);
     
+    socket.on("disconnect", () => {
+      socket.off("createdMessage", createdMessage);
+    }
+    );
   };
+export default messageHandler;
