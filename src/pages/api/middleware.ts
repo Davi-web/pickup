2beedd71b68f403f2a9e814140d1d1ebacf3cabf
Davi-web <jeseung.ha@vanderbilt.edu>
2022-11-15 04:52:33 -0600

    Adding middleware

---
+++ src/pages/api/middleware.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import NextCors from 'nextjs-cors';
+
+export async function middleware(req: NextApiRequest, res: NextApiResponse) {
+   // Run the cors middleware
+   // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+   await NextCors(req, res, {
+      // Options
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+      origin: '*',
+      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+   });
+
+   // Rest of the API logic
+   //allow socket.io connections
+   res.writeHead(200, { 
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Credentials': 'true',
+    });
+    res.end();
+}
+
+
+export const config = {
+   matcher: '/chatClass'
+};
