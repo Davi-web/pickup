af2434ee2da4ade7e8615b97c202b55fbc4b68c1
Davi-web <jeseung.ha@vanderbilt.edu>
2022-12-05 22:05:26 -0600

    final push

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,23 +49,15 @@ const Home: NextPage = () => {
             documentation="/events/eventCreation"
           />
           <Card
-            name="Join a Sports Group Chat"
-            description="Select from any of our various group chats to find a sport that you are interested in"
-            documentation="/joinGroupChat"
-          />
-          <Card
             name="Browse event postings"
             description="Within each groupchat, there is a feature to find events to meet up with your peers. You can add yourself to any event and DM other group members who are going to show up."
             documentation="/events/displayEvents"
           />
-          <Card
-            name="Meetup with other vandy students!"
-            description="Our application allows students to post events and suggest times where the most amount of people are available. RSVP for an event and meet up with your friends!"
-            documentation={`https://pickup-chat-chi.vercel.app?username=${session?.user?.email}&secret=${session?.user?.name}`}
-          />
-         
         </div>
-        <div className="flex items-center w-auto justify-center mt-8 px-8 py-4 text-white text-xl border-purple-600 border-4 rounded-full bg-purple-600 duration-500 motion-safe:hover:scale-105">
+        <div className="flex items-center w-auto justify-center mt-8 px-8 py-4 text-white text-xl border-purple-600 border-4 rounded-full bg-purple-600 duration-500 motion-safe:hover:scale-105"
+        onClick={()=>{
+          window.location.href = `https://pickup-chat-chi.vercel.app?username=${session?.user?.email}&secret=${session?.user?.name}`;
+        }}>
             <p>Are you ready to pick up sports you always wanted?</p>
             <button className="border-white border-2 rounded-full ml-10 px-4 duration-500 motion-safe:hover:scale-105">Browse Groupchats</button>
         </div>
